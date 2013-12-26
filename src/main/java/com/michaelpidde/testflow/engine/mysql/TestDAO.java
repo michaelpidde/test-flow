ff6be70c1386972ed790b4b958d3a7f1bd4a6982
Michael Pidde <michael.pidde@gmail.com>
2013-12-25 21:22:45 -0600

    Starting work on MySQL integration.

---
+++ src/main/java/com/michaelpidde/testflow/engine/mysql/TestDAO.java
@@ -0,0 +1,115 @@
+/*
+ * TestFlow Test MySQL DAO
+ *
+ * Copyright (C) 2013 Michael Pidde <michael.pidde@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package com.michaelpidde.testflow.engine.mysql;
+
+import java.util.ArrayList;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class TestDAO {
+	private static Connection connection = null;
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver").newInstance();
+		} catch(Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+
+	public static Connection connect() {
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1/testflow?user=root&password=password");
+		} catch(SQLException e) {
+			System.out.println(e.toString());
+		}
+
+		return connection;
+	}
+
+
+	public static void close() {
+		if(connection != null) {
+			try {
+				connection.close();
+			} catch(SQLException e) { }
+			connection = null;
+		}
+	}
+
+	public static ResultSet query(String sql) {
+		Statement statement = null;
+		ResultSet result = null;
+
+		try {
+			if(connection == null) {
+				connection = connect();
+			}
+			statement = connection.createStatement();
+			result = statement.executeQuery(sql);
+		} catch(SQLException e) {
+			System.out.println(e.toString());
+		}
+		
+		return result;
+	}
+
+	public static ArrayList<String> getApps() {
+		ArrayList<String> apps = new ArrayList<String>();
+		ResultSet result = null;
+
+		try {
+			result = query("select id, name from packages");
+			while(result.next()) {
+				apps.add(result.getString("name"));
+			}
+		} catch(SQLException e) {
+			System.out.println(e.toString());
+		} finally {
+			if(result != null) {
+				try {
+					result.close();
+				} catch(SQLException e) { }
+				result = null;
+			}
+
+			close();
+		}
+
+		return apps;
+	}
+
+	// public static ArrayList<String> getSuites(String app) {
+
+	// }
+
+	// public static ArrayList<String> getTests(String app) {
+
+	// }
+
+	// public static String getBaseUrl(String app) {
+
+	// }
+}
