89b82d0c76bfb4ec0246a3a3a51d464712a546d9
michael.pidde@wolfnet.com <michael.pidde@wolfnet.com>
2014-02-03 14:11:17 -0600

    Bumping the copyright date.

--- src/main/java/com/michaelpidde/testflow/engine/util/TestSuite.java
+++ src/main/java/com/michaelpidde/testflow/engine/util/TestSuite.java
@@ -1,7 +1,7 @@
 /*
  * TestFlow Selenium test suite setup and execution
  *
- * Copyright (C) 2013 Michael Pidde <michael.pidde@gmail.com>
+ * Copyright (C) 2014 Michael Pidde <michael.pidde@gmail.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
