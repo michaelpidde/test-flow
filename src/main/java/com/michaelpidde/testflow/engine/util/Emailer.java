a583f2588b220379b599d0f6642334934d0e679b
Michael <michael.pidde@gmail.com>
2015-04-23 22:33:50 -0500

    Updating copyright notice.

--- src/main/java/com/michaelpidde/testflow/engine/util/Emailer.java
+++ src/main/java/com/michaelpidde/testflow/engine/util/Emailer.java
@@ -1,20 +1,22 @@
 /*
  * TestFlow email handler
  *
- * Copyright (C) 2014 Michael Pidde <michael.pidde@gmail.com>
+ * This file is part of TestFlow.
  *
- * This program is free software; you can redistribute it and/or
+ * Copyright (C) 2014-2015 Michael Pidde <michael.pidde@gmail.com>
+ *
+ * TestFlow is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * TestFlow is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
+ * along with TestFlow; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
